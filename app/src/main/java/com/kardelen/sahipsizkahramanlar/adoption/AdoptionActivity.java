6eaa08cc42e5a41fbfe2f3a5a5aabd198799b605
Kardelen Canoğlu <85257891+kardelencanoglu@users.noreply.github.com>
2023-07-17 17:48:26 +0300

    adoption start

--- app/src/main/java/com/kardelen/sahipsizkahramanlar/adoption/AdoptionActivity.java
+++ app/src/main/java/com/kardelen/sahipsizkahramanlar/adoption/AdoptionActivity.java
@@ -1,22 +1,32 @@
 package com.kardelen.sahipsizkahramanlar.adoption;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kardelen.sahipsizkahramanlar.MainActivity;
 import com.kardelen.sahipsizkahramanlar.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-public class AdoptionActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class AdoptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ImageButton backButton;
+    Spinner species;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adoption);
 
+        species = findViewById(R.id.species);
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +36,39 @@ public class AdoptionActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        // Spinner click listener
+        species.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        species.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
