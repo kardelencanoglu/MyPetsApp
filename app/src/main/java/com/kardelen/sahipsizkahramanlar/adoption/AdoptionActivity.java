e22241624b61df6294f8c25aad6fe4db73b24d6a
Kardelen Canoğlu <85257891+kardelencanoglu@users.noreply.github.com>
2023-07-19 17:01:07 +0300

    pets add database

--- app/src/main/java/com/kardelen/sahipsizkahramanlar/adoption/AdoptionActivity.java
+++ app/src/main/java/com/kardelen/sahipsizkahramanlar/adoption/AdoptionActivity.java
@@ -16,8 +16,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kardelen.sahipsizkahramanlar.MainActivity;
 import com.kardelen.sahipsizkahramanlar.R;
 import com.kardelen.sahipsizkahramanlar.login.LoginActivity;
+import com.kardelen.sahipsizkahramanlar.otp.CreatePetPostRequestTask;
+import com.kardelen.sahipsizkahramanlar.otp.PostRequestTask;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 import de.hdodenhof.circleimageview.CircleImageView;
+import es.dmoral.toasty.Toasty;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -27,7 +30,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
 
-public class AdoptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class AdoptionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CreatePetPostRequestTask.OnPostRequestListener {
 
     ImageButton backButton;
     String name, breed, age, species, gender;
@@ -42,6 +45,8 @@ public class AdoptionActivity extends AppCompatActivity implements AdapterView.O
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_adoption);
+
+
 
         nameText = findViewById(R.id.name);
         breedText = findViewById(R.id.breed);
@@ -79,187 +84,39 @@ public class AdoptionActivity extends AppCompatActivity implements AdapterView.O
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                name = String.valueOf((nameText.getText()));
-//                breed = String.valueOf((breedText.getText()));
-//                age = String.valueOf((ageText.getText()));
-//                species = String.valueOf(speciesSpinner.getSelectedItem());
-//                gender = String.valueOf(genderSpinner.getSelectedItem());
-//                try {
-//                    // Post verileri
-//                    Map<String, String> postData = new HashMap<>();
-//                    postData.put("name", name);
-//                    postData.put("breed", breed);
-//                    postData.put("age", age);
-//                    postData.put("species", species);
-//                    postData.put("gender", gender);
-//
-//                    // Post isteği gönderme
-//                    URL url = new URL(serverUrl);
-//                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//                    connection.setRequestMethod("POST");
-//                    connection.setDoOutput(true);
-//
-//                    // Verileri isteğe eklemek için OutputStream kullanalım
-//                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-//                    outputStream.writeBytes(getPostDataString(postData));
-//                    outputStream.flush();
-//                    outputStream.close();
-//
-//                    // Sunucudan gelen cevabı okuyalım
-//                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                    String line;
-//                    StringBuilder response = new StringBuilder();
-//
-//                    while ((line = reader.readLine()) != null) {
-//                        response.append(line);
-//                    }
-//
-//                    reader.close();
-//
-//                    // Sunucudan gelen cevabı kullanın
-//                    System.out.println("Server Response: " + response.toString());
-//                    connection.disconnect();
-//                } catch (
-//                        IOException e) {
-//                    e.printStackTrace();
-//                }
-
-//                name = String.valueOf((nameText.getText()));
-//                breed = String.valueOf((breedText.getText()));
-//                age = String.valueOf((ageText.getText()));
-//                species = String.valueOf(speciesSpinner.getSelectedItem());
-//                gender = String.valueOf(genderSpinner.getSelectedItem());
-//
-//                if (!name.equals("") && !breed.equals("") && !age.equals("") && !species.equals("") && !gender.equals("")) {
-//
-//                    Handler handler = new Handler(Looper.getMainLooper());
-//                    handler.post(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            //Starting Write and Read data with URL
-//                            //Creating array for parameters
-//                            String[] field = new String[5];
-//                            field[0] = "name";
-//                            field[1] = "breed";
-//                            field[2] = "age";
-//                            field[3] = "species";
-//                            field[4] = "gender";
-//                            //Creating array for data
-//                            String[] data = new String[5];
-//                            data[0] = name;
-//                            data[1] = breed;
-//                            data[2] = age;
-//                            data[3] = species;
-//                            data[4] = gender;
-//                            PutData putData = new PutData("http://192.168.1.46/loginregister/createpet.php", "POST", field, data);
-//                            if (putData.startPut()) {
-//                                if (putData.onComplete()) {
-//                                    String result = putData.getResult();
-//                                    //End ProgressBar (Set visibility to GONE)
-//                                    Log.i("PutData", result);
-//                                    if(result.equals("Pet Save Success")){
-//                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-//                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                                        startActivity(intent);
-//                                        finish();
-//                                    }
-//                                    else {
-//                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-//                                    }
-//                                }
-//                            }
-//                            //End Write and Read data with URL
-//                        }
-//                    });
-//                }
-//                else {
-//                    Toast.makeText(getApplicationContext(), "HATA",Toast.LENGTH_SHORT).show();
-//                }
-
+                int age;
                 String name = String.valueOf(nameText.getText());
                 String breed = String.valueOf(breedText.getText());
-                String age = String.valueOf(ageText.getText());
+
+                try {
+                    age = Integer.parseInt(String.valueOf(ageText.getText()));
+                }catch (NumberFormatException e){
+                    age = 0;
+                }
+
                 String species = String.valueOf(speciesSpinner.getSelectedItem());
                 String gender = String.valueOf(genderSpinner.getSelectedItem());
 
-                NetworkTask task = new NetworkTask();
-                task.execute(name, breed, age, species, gender);
+                String ageString = age + "";
+
+                if(!name.equals("") && !breed.equals("") && !ageString.equals("") && !species.equals("") && !gender.equals("")) {
+
+                    CreatePetPostRequestTask createPetPostRequestTask = new CreatePetPostRequestTask(name,
+                            breed,
+                            age,
+                            species,
+                            gender,
+                            AdoptionActivity.this);
+                    try {
+                        createPetPostRequestTask.execute(serverUrl);
+                    } catch (IllegalStateException e) {
+                        Log.d("OTP", e + "");
+                    }
+                } else {
+                    Toasty.error(getApplicationContext(), "Please Fill All Information!", Toast.LENGTH_SHORT, true).show();
+                }
             }
         });
-    }
-
-    private class NetworkTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            String name = params[0];
-            String breed = params[1];
-            String age = params[2];
-            String species = params[3];
-            String gender = params[4];
-
-            String response = null;
-            try {
-                // Post verileri
-                Map<String, String> postData = new HashMap<>();
-                postData.put("name", name);
-                postData.put("breed", breed);
-                postData.put("age", age);
-                postData.put("species", species);
-                postData.put("gender", gender);
-
-                // Post isteği gönderme
-                URL url = new URL(serverUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setDoOutput(true);
-
-                // Verileri isteğe eklemek için OutputStream kullanalım
-                DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-                outputStream.writeBytes(getPostDataString(postData));
-                outputStream.flush();
-                outputStream.close();
-
-                // Sunucudan gelen cevabı okuyalım
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder responseBuilder = new StringBuilder();
-
-                while ((line = reader.readLine()) != null) {
-                    responseBuilder.append(line);
-                }
-
-                reader.close();
-
-                // Sunucudan gelen cevabı kullanın
-                response = responseBuilder.toString();
-                connection.disconnect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String response) {
-            // İşlem tamamlandıktan sonra sunucudan gelen cevabı kullanabilirsiniz
-            if (response != null) {
-                System.out.println("Server Response: " + response);
-            }
-        }
-    }
-
-    private String getPostDataString(Map<String, String> postData) throws IOException {
-        StringBuilder postDataString = new StringBuilder();
-        for (Map.Entry<String, String> entry : postData.entrySet()) {
-            if (postDataString.length() != 0) {
-                postDataString.append('&');
-            }
-            postDataString.append(entry.getKey());
-            postDataString.append('=');
-            postDataString.append(entry.getValue());
-        }
-        return postDataString.toString();
     }
 
 
@@ -276,6 +133,18 @@ public class AdoptionActivity extends AppCompatActivity implements AdapterView.O
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onPostRequestCompleted(String result) {
+        Log.d("TAG", "onPostRequestCompleted: " + result);
+
+
+        if(Objects.equals(result, "Pet Save Success")){
+            Toasty.success(getApplicationContext(), "Pet Save Success!", Toast.LENGTH_SHORT, true).show();
+        } else {
+            Toasty.error(getApplicationContext(), "An Unknown Error Occurred Try Again!" + result, Toast.LENGTH_SHORT, true).show();
+        }
     }
 
 }
--- app/src/main/java/com/kardelen/sahipsizkahramanlar/splash/SplashScreen.java
+++ app/src/main/java/com/kardelen/sahipsizkahramanlar/splash/SplashScreen.java
@@ -23,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                Intent i = new Intent(SplashScreen.this, AdoptionActivity.class);
                 startActivity(i);
                 finish();
             }
